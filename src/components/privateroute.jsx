e74162312d527d52ef81840bdc02553cd6e614e8
Pratistha88 <tpratistha0@gmail.com>
2025-08-02 16:26:09 +0545

    logout functionality and wrap Rooms page with layout

--- src/components/privateroute.jsx
+++ src/components/privateroute.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../context/authcontext';
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useAuth();
+  const location = useLocation();
 
   if (loading) return <div>Loading...</div>;
 
-  return user ? children : <Navigate to="/login" />;
+  return user ? (
+    children
+  ) : (
+    <Navigate to="/login" state={{ from: location }} replace />
+  );
 };
 
 export default PrivateRoute;
