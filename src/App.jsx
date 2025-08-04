b0d25bb161463d226c4a670e076851857273f947
avxxsa <153507044+avxxsa@users.noreply.github.com>
2025-08-04 19:22:52 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         
-        {/* ✅ Private chat routing */}
+        {/* Private chat routing */}
         <Route
           path="/rooms"
           element={
