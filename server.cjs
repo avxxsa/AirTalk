db728270c8b5ee99aa90f98ce7fac1672a829e04
avxxsa <153507044+avxxsa@users.noreply.github.com>
2025-08-05 20:24:32 +0545

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -100,5 +100,5 @@ function broadcastUserList() {
 
 const PORT = 3000;
 server.listen(PORT, '0.0.0.0', () => {
-  console.log(`✅ Server running at http://0.0.0.0:${PORT}`);
+  console.log(`Server running at http://0.0.0.0:${PORT}`);
 });
